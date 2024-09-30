fbd675f828db6074ed165f10e5a4ec01710cf296
potato <107034858+horyong1@users.noreply.github.com>
2024-09-30 10:53:09 +0900

    commit

---
+++ JavaWorkspace/P0930_1/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        AAA aaa = new AAA();
+        aaa.test(0);
+        
+    }
+}
+
+class AAA{
+    public void test(int value){
+        
+    }
+}
