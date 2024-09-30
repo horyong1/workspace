70f8aca63ae6131b7158b2bb148e3fab83adf9ca
potato <107034858+horyong1@users.noreply.github.com>
2024-09-30 11:12:59 +0900

    enum 설명

--- JavaWorkspace/P0930_2/src/p1/App.java
+++ JavaWorkspace/P0930_2/src/p1/App.java
@@ -2,7 +2,45 @@ package p1;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello");
+        // enum = 값의 범위를 제한(도메인 제한);
+        // String gender = "F";
+        // int gender = 0;
+        // 0 = 남자
+        // 1 = 여자
+        Days days = Days.FRIDAY;
+
+
     }
+}
+enum Days{
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+}
+
+enum Gender{
+    MALE("남자",0),
+    FEMALE("여자",1);
+
+    private String gender;
+    private int value;
+
+    Gender(String gender, int value){
+        this.gender = gender;
+        this.value = value;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    
 }
 
