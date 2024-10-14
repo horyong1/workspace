b1e024c231c2c38ac7620ea3287e622f21709ed2
potato <107034858+horyong1@users.noreply.github.com>
2024-10-14 15:32:33 +0900

    boardController 생성

---
+++ JavaWorkspace/board/src/controller/boardController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import ioutil.Ioutil;
+
+public class boardController {
+    public void run(){
+        hello();
+
+        while (true) {
+            showMenu();
+            
+            String key = Ioutil.input("커맨드 입력 >>> ");
+            if(key.equals("0")){
+                break;
+            }
+            command(key);
+        }
+        bye();
+    }
+
+    private void hello(){
+        Ioutil.print("===================");
+        Ioutil.print("===================");
+        Ioutil.print("===헛소리 게시판===");
+        Ioutil.print("===================");
+        Ioutil.print("===================");
+    }
+    private void showMenu(){
+        Ioutil.print("1.게시판 접속");
+        Ioutil.print("2.회원가입");
+        Ioutil.print("0.게시판 종료");
+        
+    }
+
+    private void bye(){
+        Ioutil.print("프로그램을 종료 합니다.");
+    }
+
+    private void command(String key){
+        switch (key) {
+            case "1":
+                Ioutil.pause();
+                break;
+            case "2":
+                Ioutil.pause();
+                break;
+            default:
+                Ioutil.print("잘못된 입력입니다.");
+                Ioutil.pause();
+                break;
+        }
+    }
+
+}
