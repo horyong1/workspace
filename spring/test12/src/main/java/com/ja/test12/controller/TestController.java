6b79a9a9fe34dd88b9a63ae978131544bf158297
potato <107034858+horyong1@users.noreply.github.com>
2024-10-24 14:34:27 +0900

    프로젝트 생성

---
+++ spring/test12/src/main/java/com/ja/test12/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.ja.test12.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class TestController {
+    
+    @GetMapping("/hello")
+    @ResponseBody
+    public String getMethodName() {
+        return "Helloworsdsadassld";
+    }
+    
+}
---
+++ spring/test12/src/main/java/com/ja/test12/service/TestService.java
@@ -0,0 +1,11 @@
+package com.ja.test12.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public void someMethod(){
+        
+    }
+}
