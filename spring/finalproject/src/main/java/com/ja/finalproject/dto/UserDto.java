409027bf15077b01f3a7303b0eb0f3521eba1ade
potato <107034858+horyong1@users.noreply.github.com>
2024-10-28 16:18:12 +0900

    UserDto 필드 추가 및 date 포멧 설정

--- spring/finalproject/src/main/java/com/ja/finalproject/dto/UserDto.java
+++ spring/finalproject/src/main/java/com/ja/finalproject/dto/UserDto.java
@@ -1,12 +1,23 @@
 package com.ja.finalproject.dto;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class UserDto {
     private int id;
-    private String email;
+    private String userId;
     private String password;
     private String nickname;
-    private String registerdate;
+    private String email;
+    private String gender;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birth;
+
+    private String phone;
+    private Date createdAt;
 }
