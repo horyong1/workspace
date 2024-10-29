d592a3c2990a9641ec401e967232af64796c7821
potato <107034858+horyong1@users.noreply.github.com>
2024-10-29 17:25:05 +0900

    연습용 프로젝트 생성

---
+++ spring/study1029/src/main/java/com/ja/study/study1029/Study1029Application.java
@@ -0,0 +1,13 @@
+package com.ja.study.study1029;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study1029Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study1029Application.class, args);
+	}
+
+}
---
+++ spring/study1029/src/test/java/com/ja/study/study1029/Study1029ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ja.study.study1029;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study1029ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
