6a92e15e925155735a1edadb0e21d12ae8b9786d
potato <107034858+horyong1@users.noreply.github.com>
2024-11-06 16:22:08 +0900

    2차 Ajax 비동기 사용법

--- spring/finalproject/src/main/java/com/ja/finalproject/board/controller/TestController.java
+++ spring/finalproject/src/main/java/com/ja/finalproject/board/controller/TestController.java
@@ -1,15 +1,26 @@
 package com.ja.finalproject.board.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.ja.finalproject.board.service.BoardService;
+
 import jakarta.servlet.http.HttpSession;
 
 
-@Controller
+// 여기서 사용되는 api 는 restAPI 임
+@RestController
 public class TestController {
+
+    @Autowired
+    private BoardService boardService;
 
     @RequestMapping("test1")
     public String test1(HttpSession session, Model model){
@@ -21,5 +32,10 @@ public class TestController {
 
         model.addAttribute("qwer", message);
         return "test/test";
+    }
+
+    @RequestMapping("api/test1")
+    public List<Map<String,Object>> test1(){
+        return boardService.getArticleList(null, null, 1);
     }
 }
